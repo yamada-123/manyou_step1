cc632afb74426335b9c9856bd4ab44eee90dc794
satoshi yamada <yasain0852@yahoo.co.jp>
2019-12-24 12:15:23 +0900

    アプリ初期作成

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  config.load_defaults 5.1
+  config.i18n.default_locale = :ja
+  config.time_zone = 'Tokyo'
+  config.generators do |g|
+    g.assets false
+    g.helper false
+    g.jbuilder false
+  end
+end
