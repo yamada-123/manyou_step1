5918b6b3fa584f894373bf82162582899fdb4a81
satoshi yamada <yasain0852@yahoo.co.jp>
2019-12-25 13:27:05 +0900

    登録機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,53 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+
+    redirect_to tasks_path, notice: "タスクを登録しました"
+    else
+    render :new
+    end
+  end
+
+  def show
+   @task = Task.find(params[:id])
+  end
+
+  def edit 
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスクを削除しました"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
