130a20e9dc30db10af1aa0bf90666726d1a67fc7
satoshi yamada <yasain0852@yahoo.co.jp>
2020-01-13 11:28:01 +0900

    heroku

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module ManyouStep1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # the framework and any gems in youar application.
     config.generators do |g|
       g.test_framework :rspec,
                        model_specs: true,
